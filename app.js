c96e675ce410280b9e9cebdfccfe4539f17b749e
superstar1003 <chandra.subhash61@gmail.com>
2021-02-18 08:03:31 +0530

    comments added

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
+//Serving static files
 app.use(express.static(path.join(__dirname, `public`)));
 
 //Body Parser
 app.use(express.json());
 
+//Routes
 app.use("/api/v1/resturants/", resturantRouter);
 
 app.use(globalErrorHandler);
